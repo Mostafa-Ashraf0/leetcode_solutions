542c57f54350a4a0559b0ae90f33b9e308414ef4
mostafa ashraf <mostafaashrof158@gmail.com>
2026-07-21 22:28:11 +0300

    the ideal solution

--- 49. Group Anagrams.cpp
+++ 49. Group Anagrams.cpp
@@ -1,3 +1,45 @@
+// the ideal solution with hashmap of key(sorted string of the strs array) and value of anagrams array
+
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>> finalList;
+        unordered_map<string, vector<string>> finalMap;
+        string currentString;
+        string finalString;
+        vector<char> currentStringArray;
+
+
+        for(int i = 0; i < strs.size(); i++){
+            currentString = strs[i];
+            // push elelments to the array
+            for(int j = 0; j < currentString.length(); j++){
+                currentStringArray.push_back(currentString[j]);
+            }
+            // sort elements
+            sort(currentStringArray.begin(), currentStringArray.end()); 
+
+            // store elements in one string ordered
+            string finalString(currentStringArray.begin(), currentStringArray.end());
+            // pop elements from the array
+            for(int j = 0; j < currentString.length(); j++){
+                currentStringArray.pop_back();
+            }
+            finalMap[finalString].push_back(currentString);
+        }
+
+        for (auto it = finalMap.begin(); it != finalMap.end(); it++) {
+            finalList.push_back(it->second);
+        }
+
+        return finalList;
+    }
+};
+
+
+
+
+
 // my first solution (brute force) but it refused because of (time limit issue, o(n2 * k) time complexity) 
 
 class Solution {
@@ -55,3 +97,5 @@ public:
         return finalList;
     }
 };
+
+
