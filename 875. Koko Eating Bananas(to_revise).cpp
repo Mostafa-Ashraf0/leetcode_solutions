a1b960b780f2d408d587ea41b51a30a1d5683174
mostafa ashraf <mostafaashrof158@gmail.com>
2026-08-14 01:32:42 +0300

    koko eating bananas

---
+++ 875. Koko Eating Bananas(to_revise).cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        sort(piles.begin(), piles.end());
+        int low = 1;
+        int high = piles[piles.size()-1];
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            long long hours = 0;
+
+            for (int pile : piles) {
+                hours += (pile + mid - 1) / mid;
+            }
+
+            if (hours <= h) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return low;
+    }
+};
